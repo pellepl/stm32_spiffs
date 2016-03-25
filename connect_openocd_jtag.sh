7c9f45dbeb65b39b26a0644797eaf61cbe0c984b
Peter Andersson <pelleplutt1976@gmail.com>
2016-03-25 21:52:33 +0100

    fixed openocd cfg

---
+++ connect_openocd_jtag.sh
@@ -0,0 +1,2 @@
+/home/petera/opt/openocd/bin/openocd -s /home/petera/opt/openocd/share/openocd/scripts -f openocd/spiffs_stm32_jtag.cfg
+
--- connect_openocd_stlinkv2.sh
+++
@@ -1,2 +0,0 @@
-/home/petera/opt/openocd/bin/openocd -s /home/petera/opt/openocd/share/openocd/scripts -f openocd/wisleep_stlinkv2.cfg
-
